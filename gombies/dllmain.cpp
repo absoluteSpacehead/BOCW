748ec59785741bbe384055dd372496384967a277
Alexander <87954949+absoluteSpacehead@users.noreply.github.com>
2025-01-18 15:42:34 +0000

    cleanup

--- gombies/dllmain.cpp
+++ gombies/dllmain.cpp
@@ -1,7 +1,6 @@
 // gombies.dll: inject in the multiplayer menu to switch to zombies.
 
 #include "pch.h"
-#include <cstdint>
 
 template<class T>
 T* Offset(uintptr_t offset)
@@ -9,13 +8,11 @@ T* Offset(uintptr_t offset)
     return reinterpret_cast<T*>(reinterpret_cast<uintptr_t>(GetModuleHandle(0)) + offset);
 }
 
-typedef uint64_t QWORD;
-
-void (*Cbuf_AddText)(QWORD param_1, const char* param_2) = nullptr;
+void (*Cbuf_AddText)(uintptr_t param_1, const char* param_2) = nullptr;
 
 DWORD MainThread(LPVOID hModule)
 {
-    Cbuf_AddText = Offset<void(QWORD, const char*)>(0x16F3A10);
+    Cbuf_AddText = Offset<void(uintptr_t, const char*)>(0x16F3A10);
 
     *Offset<DWORD>(0x65259E4) = 0x40C0;
     *Offset<DWORD>(0xB7D18A0) = 0;
